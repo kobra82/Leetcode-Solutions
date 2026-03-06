87abf36c4e2164c8d6a65de8e9b19196d65fed18
Giovanni Cuva <kobra82@gmail.com>
2026-03-06 11:05:46 +0100

    invio problemi risolti il 12 febbraio 2026

---
+++ Python3/Easy/countMonobit.py
@@ -0,0 +1,42 @@
+# You are given an integer n.
+
+# An integer is called Monobit if all bits in its binary representation are the same.
+
+# Return the count of Monobit integers in the range [0, n] (inclusive).
+
+ 
+
+# Example 1:
+
+# Input: n = 1
+
+# Output: 2
+
+# Explanation:
+
+# The integers in the range [0, 1] have binary representations "0" and "1".
+# Each representation consists of identical bits. Thus, the answer is 2.
+# Example 2:
+
+# Input: n = 4
+
+# Output: 3
+
+# Explanation:
+
+# The integers in the range [0, 4] include binaries "0", "1", "10", "11", and "100".
+# Only 0, 1 and 3 satisfy the Monobit condition. Thus, the answer is 3.
+ 
+
+# Constraints:
+
+# 0 <= n <= 1000
+
+class Solution:
+    def countMonobit(self, n: int) -> int:
+        monobit = 0
+        for i in range(n+1):
+            bini = format(i,'b')
+            if (bini.count('1') == len(bini) or bini.count('0') == len(bini)):
+                monobit+=1
+        return monobit
