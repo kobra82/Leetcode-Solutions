4d98b5df0c5723998351841693a028d99af03a00
Giovanni Cuva <kobra82@gmail.com>
2026-03-06 10:50:33 +0100

    invio soluzione problema easy numero 9

--- Python3/Easy/Two-Sum.py
+++ Python3/Easy/Two-Sum.py
@@ -32,7 +32,7 @@
 # Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if (nums[i]+nums[j]==target):
