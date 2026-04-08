3c169ce1a6974e95fd2a55cf42a4532255aaed97
Giovanni Cuva <kobra82@gmail.com>
2026-04-08 11:26:09 +0200

    invio problemi risolti il 8 Aprile 2026 sessione mattutina

---
+++ Python3/TopInterview150/000242validAnagram.py
@@ -0,0 +1,42 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "anagram", t = "nagaram"
+
+# Output: true
+
+# Example 2:
+
+# Input: s = "rat", t = "car"
+
+# Output: false
+
+ 
+
+# Constraints:
+
+# 1 <= s.length, t.length <= 5 * 104
+# s and t consist of lowercase English letters.
+ 
+
+# Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+
+        for char in t:
+            if char not in count or count[char] == 0:
+                return False
+            count[char] -= 1
+
+        return True
