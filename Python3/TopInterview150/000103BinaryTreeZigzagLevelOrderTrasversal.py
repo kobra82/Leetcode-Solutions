ab70b80f02eef57d1cb6ade7bd6e87f8cde25380
Giovanni Cuva <kobra82@gmail.com>
2026-04-15 12:23:15 +0200

    invio problemi risolti il 15 Aprile 2026 sessione pomeridiana

---
+++ Python3/TopInterview150/000103BinaryTreeZigzagLevelOrderTrasversal.py
@@ -0,0 +1,59 @@
+# Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+
+ 
+
+# Example 1:
+
+
+# Input: root = [3,9,20,null,null,15,7]
+# Output: [[3],[20,9],[15,7]]
+# Example 2:
+
+# Input: root = [1]
+# Output: [[1]]
+# Example 3:
+
+# Input: root = []
+# Output: []
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [0, 2000].
+# -100 <= Node.val <= 100
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> list[list[int]]:
+        if not root:
+            return []
+        
+        result = []
+        queue = [root]
+        left_to_right = True
+        
+        while queue:
+            level_size = len(queue)
+            level_values = []
+            
+            for _ in range(level_size):
+                node = queue.pop(0)
+                level_values.append(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            if not left_to_right:
+                level_values.reverse()
+            
+            result.append(level_values)
+            left_to_right = not left_to_right
+        
+        return result
