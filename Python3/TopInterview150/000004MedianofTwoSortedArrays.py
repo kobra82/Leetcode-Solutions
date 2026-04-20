42d4ddd654cb08a785ef32f854150e3dc7a9c8da
Giovanni Cuva <kobra82@gmail.com>
2026-04-20 10:21:08 +0200

    invio problemi risolti il 20 Aprile 2026 sessione mattuttina

---
+++ Python3/TopInterview150/000004MedianofTwoSortedArrays.py
@@ -0,0 +1,52 @@
+# Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+# The overall run time complexity should be O(log (m+n)).
+
+ 
+
+# Example 1:
+
+# Input: nums1 = [1,3], nums2 = [2]
+# Output: 2.00000
+# Explanation: merged array = [1,2,3] and median is 2.
+# Example 2:
+
+# Input: nums1 = [1,2], nums2 = [3,4]
+# Output: 2.50000
+# Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ 
+
+# Constraints:
+
+# nums1.length == m
+# nums2.length == n
+# 0 <= m <= 1000
+# 0 <= n <= 1000
+# 1 <= m + n <= 2000
+# -106 <= nums1[i], nums2[i] <= 106
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        merged = []
+        i, j = 0, 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                merged.append(nums1[i])
+                i += 1
+            else:
+                merged.append(nums2[j])
+                j += 1
+
+        while i < len(nums1):
+            merged.append(nums1[i])
+            i += 1
+
+        while j < len(nums2):
+            merged.append(nums2[j])
+            j += 1
+
+        n = len(merged)
+        if n % 2 == 0:
+            return (merged[n // 2 - 1] + merged[n // 2]) / 2
+        else:
+            return merged[n // 2]
---
+++ Python3/TopInterview150/000034FindFirstandLastPositionofElementinSortedArray.py
@@ -0,0 +1,58 @@
+# Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+# If target is not found in the array, return [-1, -1].
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+# Example 1:
+
+# Input: nums = [5,7,7,8,8,10], target = 8
+# Output: [3,4]
+# Example 2:
+
+# Input: nums = [5,7,7,8,8,10], target = 6
+# Output: [-1,-1]
+# Example 3:
+
+# Input: nums = [], target = 0
+# Output: [-1,-1]
+ 
+
+# Constraints:
+
+# 0 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+# nums is a non-decreasing array.
+# -109 <= target <= 109
+
+class Solution:
+    def searchRange(self, nums: list[int], target: int) -> list[int]:
+        def binarySearchLeft(nums, target):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return left
+
+        def binarySearchRight(nums, target):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] <= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return right
+
+        leftIndex = binarySearchLeft(nums, target)
+        rightIndex = binarySearchRight(nums, target)
+
+        if leftIndex <= rightIndex and rightIndex < len(nums) and nums[leftIndex] == target and nums[rightIndex] == target:
+            return [leftIndex, rightIndex]
+        else:
+            return [-1, -1]
